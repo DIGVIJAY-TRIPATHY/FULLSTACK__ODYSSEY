f5b19da3105eafc07c0a5c6ee0090088b11a50e4
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-07-04 01:00:27 +0530

    feat: implement prototype chaining with Animal and Dog constructors & task 1 completed

---
+++ notes/JS-foundation/task/PrototypesTask.js
@@ -0,0 +1,33 @@
+/*
+Prototypes in JavaScript
+Task: Prototype Chaining
+Create a constructor function Animal that has a method speak() that return 'Animal speaking'.
+Then create another constructor Dog that inherits from Animal using prototypes.
+The Dog constructor should add a method bark() that returns 'Woof!'. Demonstrate the prototype chain between Dog and Animal. 
+*/
+
+
+function Animal() {}
+
+Animal.prototype.speak = function() {
+    return 'Animal speaking';
+}
+
+function Dog() {}
+
+Dog.prototype = Object.create(Animal.prototype)
+
+Dog.prototype.bark = function() {
+    return 'Woof!';
+}
+
+const myDog = new Dog();
+
+console.log(myDog.speak()); // Inherited from Animal -> "Animal speaking"
+console.log(myDog.bark());  // Defined in Dog -> "Woof!"
+
+// Step 4: Verify prototype chain
+console.log(myDog instanceof Dog);    // true
+console.log(myDog instanceof Animal); // true
+console.log(Object.getPrototypeOf(Dog.prototype) === Animal.prototype); // true
+
