4bee3ee71ff7a44b0e99b1f7cce6c83f7c204cb7
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-07-15 12:34:06 +0530

    feat: add PrototypalInheritanceinJavaScript.js with Animal, Dog, Shape, and Rectangle classes

---
+++ notes/JS-foundation/part8_adv/task/PrototypalInheritanceinJavaScript.js
@@ -0,0 +1,34 @@
+function Animal() {}
+ 
+Animal.prototype.makeSound = function() {
+  return 'Animal sound';
+};
+ 
+function Dog() {}
+ 
+Dog.prototype = Object.create(Animal.prototype);
+ 
+Dog.prototype.bark = function() {
+  return 'Woof!';
+};
+ 
+Dog.prototype.constructor = Dog;
+ 
+function Shape(color) {
+  this.color = color;
+}
+Shape.prototype.getColor = function() {
+  return this.color;
+};
+ 
+function Rectangle(width, height, color) {
+  Shape.call(this, color); // Call the Shape constructor
+  this.width = width;
+  this.height = height;
+}
+Rectangle.prototype = Object.create(Shape.prototype); // Set up inheritance
+Rectangle.prototype.constructor = Rectangle;
+ 
+Rectangle.prototype.getArea = function() {
+  return this.width * this.height;
+};
