42ad2dd5fbe29cb999abfc0e9f87ad1782a1a3f8
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-07-15 21:22:54 +0530

    feat: add initial HTML, CSS, and JavaScript files for Dark Mode To-Do List application

---
+++ projects/js🔻/01_todo-localstorage/script.js
@@ -0,0 +1,20 @@
+const todoInput = document.getElementById("todo-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const todoList = document.getElementById("todo-list");
+
+let tasks = []
+
+addTaskBtn.addEventListener("click", () => {
+    const taskText = todoInput.value.trim();
+    if (taskText === "") return
+
+    const newTask = {
+        id: Date.now(),
+        text: taskText,
+        completed: false
+    }
+    tasks.push(newTask)
+    todoInput.value = "";
+    console.log(tasks);
+    
+});
