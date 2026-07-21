0eec20855865b689cf834177a96658f2d86973d4
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-07-21 23:52:55 +0530

    feat: implement event logging functionality with Logger class

---
+++ notes/NODEJS/eventLogger/logger.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const os = require('os')
+
+const EventEmitter = require('events')
+
+const Logger extends EventEmitter {
+    log(message) {
+        this.emit('message', {message})
+    }
+}
+
+const logger = new Logger()
+const logFile = './eventlog.txt'
+
+const logToFile = (event) => {
+    const logMessage = `${new Date().toISOString()} - ${event.message} \n`
+
+    fs.appendFileSync(logFile, logMessage)
+}
+
+logger
+    .on('message', logToFile)
