6c7d862576ef2a3e4a4a5e305898b0ae9d0411e1
DIGVIJAY TRIPATHY <tripathydigvijay7377@gmail.com>
2026-08-22 09:32:54 +0530

    feat: add URL module for request parsing and update dependencies

--- notes/NODEJS-full/basic2/index.js
+++ notes/NODEJS-full/basic2/index.js
@@ -1,15 +1,21 @@
 import http from 'http'
 import fs from 'fs'
-
+import url from 'url'
 
 const app = http.createServer((req, res) => {
-    
+    if (req.url === '/favicon.ico') return res.end()
+
     // lets append teh ip address of the user
     const log = `${Date.now()}: ${req.url} ${req.method} and the ip is ${req.socket.remoteAddress} \n`
 
     fs.appendFile("log.txt", log, () => {
         res.end("hello from server")
     })
+
+    const myUrl = url.parse(req.url)
+
+    console.log(myUrl);
+    
     
 })
 
